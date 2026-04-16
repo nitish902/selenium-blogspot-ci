03a36f4c611cb7c06d83d81f32d4cedd7cdb55b8
Nitish Shukla <nitishshukla902@gmail.com>
2026-04-16 15:42:36 +0530

    Refactor test_form to use page object model
    Refactor test_form to use FormPage class for better structure.

--- tests/test_form.py
+++ tests/test_form.py
@@ -1,16 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import Select, WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver import ActionChains
-import time
+from pages.form_page import FormPage
 
 def test_form():
 
     chrome_options = Options()
+
     chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
@@ -23,45 +20,20 @@ def test_form():
 
     driver.get("https://testautomationpractice.blogspot.com/")
 
-    wait = WebDriverWait(driver,10)
+    form = FormPage(driver)
 
-    wait.until(EC.presence_of_element_located((By.ID,"name"))).send_keys("Nitish")
+    form.enter_name("John")
 
-    driver.find_element(By.ID,"email").send_keys("test@gmail.com")
+    form.enter_email("test@gmail.com")
 
-    driver.find_element(By.ID,"phone").send_keys("9999999999")
+    form.enter_phone("9999999999")
 
-    driver.find_element(By.ID,"male").click()
+    form.select_gender()
 
-    driver.find_element(By.ID,"sunday").click()
+    form.select_day()
 
-    country = Select(driver.find_element(By.ID,"country"))
-    country.select_by_visible_text("India")
+    form.select_country("India")
 
-    driver.find_element(By.ID,"datepicker").send_keys("04/16/2026")
-
-    driver.execute_script("window.scrollBy(0,800)")
-
-    alert_btn = wait.until(
-        EC.element_to_be_clickable((By.XPATH,"//button[contains(text(),'Alert')]"))
-    )
-
-    alert_btn.click()
-
-    alert = driver.switch_to.alert
-    alert.accept()
-
-    driver.execute_script("window.scrollBy(0,500)")
-
-    table_value = driver.find_element(By.XPATH,"//table[@name='BookTable']/tbody/tr[2]/td[1]").text
-
-    assert table_value == "Learn Selenium"
-
-    driver.execute_script("window.scrollBy(0,500)")
-
-    source = driver.find_element(By.ID,"draggable")
-    target = driver.find_element(By.ID,"droppable")
-
-    ActionChains(driver).drag_and_drop(source,target).perform()
+    form.select_date("04/16/2026")
 
     driver.quit()
